5d075dd9b96da9070b562c1ddda1884096114e6b
Felipe <felipeselau68@gmail.com>
2023-05-23 18:58:16 -0300

    implemented tiny-slider on tech stack carousel

--- 1.HTML-CSS/src/scripts/slider.js
+++ 1.HTML-CSS/src/scripts/slider.js
@@ -1,7 +1,15 @@
+import { tns } from "../../../node_modules/tiny-slider/src/tiny-slider.js"
 
 let slider = tns({
-    container: '.galery-wrapper',
+    container: '.galery',
     items: 6,
     slideBy: 1,
-    autoplay: true
+    autoplay: true,
+    autoplayButton: false,
+    autoplayButtonOutput: false,
+    nav: false,
+    controls: false,
+    autoplayTimeout: 1000,
+    loop: true,
+    speed: 1000
 })
